acd2a94f3c0ccffe693461657a0fc21b89855c49
Dmitry Kovalenko <peppercofee@gmail.com>
2021-03-19 01:02:59 +0700

    add line rotation

--- js_lens.js
+++ js_lens.js
@@ -14,29 +14,95 @@ let circlePoints = {};
 let bezierPoints = [];
 const pointSize = 15;
 
-let mainPlane = SVG().addTo("body").size(600, 600);
+let mainPlane = SVG("#main-plane");
 
 // eslint-disable-next-line no-unused-vars
-function createLine() {
-    let yCoords = [];
-    for (let index in circlePoints) {
-        yCoords.push(circlePoints[index].y);
+function createLine(event) {
+    let mainPlaneElem = document.getElementById("main-plane");
+    let yOffset = mainPlaneElem.getBoundingClientRect().y;
+    let x = event.clientX;
+    let y = event.clientY - yOffset;
+    let halfHeight = 105;
+    let line = document.getElementById("line");
+    if (line)
+        line.parentNode.removeChild(line);
+
+    line = mainPlane.line(x, y - halfHeight,
+                          x, y + halfHeight)
+                    .stroke({ width: 2, color: '#00a0f0' })
+                    .attr({ id: "line" });
+    
+    mainPlaneElem.addEventListener("mousemove", mouseMoveLine, false);
+    mainPlaneElem.addEventListener("wheel", rotateOnScroll, false);
+    mainPlaneElem.addEventListener("mouseup", mouseUp, false);
+
+    function mouseMoveLine(event) {
+        let lineCoords = line.array();
+        let x = event.clientX;
+        let y = event.clientY - yOffset;
+
+        let yTop, yBot, xTop, xBot;
+        if (lineCoords[0][1] < lineCoords[1][1]) {
+            xTop = lineCoords[0][0];
+            yTop = lineCoords[0][1];
+            xBot = lineCoords[1][0];
+            yBot = lineCoords[1][1];
+        }
+        else {
+            xTop = lineCoords[1][0];
+            yTop = lineCoords[1][1];
+            xBot = lineCoords[0][0];
+            yBot = lineCoords[0][1];
+        }
+
+        let xDiff = xTop - xBot;
+        let yDiff = yBot - yTop;
+        xTop = x + xDiff / 2;
+        xBot = x - xDiff / 2;
+        yTop = y - yDiff / 2;
+        yBot = y + yDiff / 2;
+        // document.getElementById("coords").innerHTML = `${xTop.toFixed(2)}, ${yTop.toFixed(2)}; ${xBot.toFixed(2)}, ${yBot.toFixed(2)}`;
+        
+        let startPoint = circlePoints[1];
+        let endPoint = circlePoints[4];
+
+        let distance1 = Math.pow((startPoint.x - xTop)**2 + (startPoint.y - yTop)**2, 0.5);
+        let distance2 = Math.pow((endPoint.x - xBot)**2 + (endPoint.y - yBot)**2, 0.5);
+        if (distance1 < pointSize / 2 && distance2 < pointSize / 2) {
+            line.plot(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+            return;
+        }
+        line.plot(xTop, yTop, xBot, yBot);
     }
-    if (yCoords.length < 2)
-        return;
 
-    let [minIdx, maxIdx] = argminAndArgmax(yCoords);
-    let highPoint = circlePoints[minIdx + 1];
-    let lowPoint = circlePoints[maxIdx + 1];
+    function rotateOnScroll(event) {
+        let lineCoords = line.array();
+        let x = event.clientX;
+        let y = event.clientY - yOffset;
+        let yTop = lineCoords[0][1] - y;
+        let xTop = lineCoords[0][0] - x;
+        let yBot = lineCoords[1][1] - y;
+        let xBot = lineCoords[1][0] - x;
+        let angle = 2 / 180 * Math.PI;
+        angle = event.deltaY < 0 ? angle : -angle;
 
-    mainPlane.line(highPoint.x + pointSize / 2, highPoint.y + pointSize / 2, 
-                   lowPoint.x + pointSize / 2, lowPoint.y + pointSize / 2)
-             .stroke({ width: 2, color: '#00a0f0' })
-             .attr({ id: "line" });
+        let newXTop = xTop * Math.cos(angle) - yTop * Math.sin(angle) + x;
+        let newYTop = xTop * Math.sin(angle) + yTop * Math.cos(angle) + y;
+        let newXBot = xBot * Math.cos(angle) - yBot * Math.sin(angle) + x;
+        let newYBot = xBot * Math.sin(angle) + yBot * Math.cos(angle) + y;
+        line.plot(newXTop, newYTop, newXBot, newYBot);
+    }
+
+    function mouseUp() {
+        mainPlaneElem.removeEventListener("mousemove", mouseMoveLine, false);
+        mainPlaneElem.removeEventListener("wheel", rotateOnScroll, false);
+        mainPlaneElem.removeEventListener("mouseup", mouseUp, false);
+        // document.getElementById("coords").innerHTML = event.clientX + ", " + event.clientY;
+    }
 }
 
 // eslint-disable-next-line no-unused-vars
-function onClickfrontCurve() {
+function onClickFrontCurve() {
     let pointsLength = Object.keys(circlePoints).length + 1;
     let len_not_1_or_4 = (pointsLength - 1) * (pointsLength - 4);
     let offset = 0;
@@ -47,10 +113,9 @@ function onClickfrontCurve() {
     let x = 100 + offset;
     let y = pointsLength * 70 + 10;
 
-    if (pointsLength <= 4)
-        circlePoints[pointsLength] = new Point(x, y);
-    else
+    if (pointsLength > 4)
         return;
+    circlePoints[pointsLength] = new Point(x, y);
 
     drawPoints(x, y, pointsLength);
 }
@@ -59,8 +124,8 @@ function drawPoints(x, y, pointsLength) {
     const strokeWidth = 1;
 
     let attributes = {
-        cx: x + pointSize / 2,
-        cy: y + pointSize / 2,
+        cx: x,
+        cy: y,
         fill: "white",
         stroke: "#00f0a0",
         "stroke-width": strokeWidth,
@@ -76,15 +141,14 @@ function drawPoints(x, y, pointsLength) {
     if (bezierPoints.length == 0)
         return;
     
-    let o = pointSize / 2;
-    let frontPathD = `M ${circlePoints[1].x + o} ${circlePoints[1].y + o} `;
-    let point2 = `${circlePoints[2].x + o} ${circlePoints[2].y + o} `;
-    let point3 = `${circlePoints[3].x + o} ${circlePoints[3].y + o} `;
+    let frontPathD = `M ${circlePoints[1].x} ${circlePoints[1].y} `;
+    let point2 = `${circlePoints[2].x} ${circlePoints[2].y} `;
+    let point3 = `${circlePoints[3].x} ${circlePoints[3].y} `;
     if (pointsLength == 3) {
         frontPathD += "Q " + point2 + point3;
     }
     else if (pointsLength == 4) {
-        let point4 = `${circlePoints[4].x + o} ${circlePoints[4].y + o}`;
+        let point4 = `${circlePoints[4].x} ${circlePoints[4].y}`;
         frontPathD += "C " + point2 + point3 + point4;
     }
     else
@@ -146,9 +210,7 @@ function circleDrag(event) {
     let idx = Math.floor(circleIdx / 4);
     lineCoords[idx][0] = x;
     lineCoords[idx][1] = y;
-    lineCoords = lineCoords[0].concat(lineCoords[1]);
-
-    line.plot(...lineCoords);
+    line.plot([lineCoords[0], lineCoords[1]]);
 }
 
 function calcBezierPoints(pointsLength) {
